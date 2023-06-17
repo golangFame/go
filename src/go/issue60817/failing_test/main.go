6b588f525be0bcddcd831df2f29f346a0393bff0
Hiro <laciferin@gmail.com>
2023-06-17 18:31:57 +0530

    remove: fmt

--- src/go/issue60817/failing_test/main.go
+++ src/go/issue60817/failing_test/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // It doesn't matter if the innerT struct unexported or exported, the result is the same.
 // It also doesn't matter if the R type parameter is infer to a pointer or not, the result is the same.
 type innerT[T any, R *T1[T] | *T2[T]] struct {
@@ -13,5 +11,4 @@ type T1[T any] struct {
 }
 
 func main() {
-	fmt.Println("doesn't panic")
 }
